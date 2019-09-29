f8b79dd470bf7b57fd336571db87cef0639308a8
cqs <494053286@qq.com>
2019-09-29 19:47:48 +0800

    完善主页页面 axios读取静态资源里json

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import 'assets/css/reset.css'
-
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
