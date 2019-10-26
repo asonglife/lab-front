969fefbfee6ecb9099b2902611c40a3ac3a0442b
cqs <494053286@qq.com>
2019-10-26 22:02:40 +0800

    优化资产登记表单页面

--- src/view/backend/system/manage/managecaptial.vue
+++ src/view/backend/system/manage/managecaptial.vue
@@ -12,21 +12,32 @@
           <el-table-column prop="address" label="备注" :show-overflow-tooltip="true"></el-table-column>
         </el-table>
       </el-main>
-      <el-aside width="250px" style="padding:18px">
-        <el-form>
-          <el-form-item>
-            <el-input placeholder="登记项目"></el-input>
+      <el-aside width="290px" style="padding:18px;">
+        <h6>资产登记填写</h6>
+        <el-form style="margin:24px;">
+          <el-form-item ref="captialform" :model="captial">
+            <el-select placeholder="登记项目">
+              <el-option label="电脑" value="shanghai"></el-option>
+              <el-option label="实验器材" value="beijing"></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="登记金额"></el-input>
+            <el-input placeholder="登记金额" suffix-icon="el-icon-coin"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="登记人"></el-input>
+            <el-input placeholder="登记人" suffix-icon="el-icon-user"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input placeholder="备注"></el-input>
+            <el-col :span="11">
+              <el-date-picker type="date" placeholder="选择登记日期"></el-date-picker>
+            </el-col>
           </el-form-item>
+          <el-form-item>
+            <el-input type="textarea" placeholder="备注"></el-input>
+          </el-form-item>
+
           <el-button type="primary" plain>提交</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
         </el-form>
       </el-aside>
     </el-container>
