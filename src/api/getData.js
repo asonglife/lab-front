f4959e8eaacb8fda5c6c8be4dbb012860bddac9e
cqs <494053286@qq.com>
2019-10-30 01:22:49 +0800

    对获取数据的响应过程中做了处理

--- src/api/getData.js
+++ src/api/getData.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
-
+import {
+  Loading
+}
+  from 'element-ui'
 const getService = axios.create({
   baseURL: ''
 })
-
 getService.interceptors.request.use(
   function (request) {
+    window.LoadingInstance = Loading.service({
+      text: 'Loading',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)'
+    })
     return Promise.resolve(request)
   },
   function (error) {
@@ -14,6 +21,7 @@ getService.interceptors.request.use(
 )
 getService.interceptors.response.use(
   function (response) {
+    window.LoadingInstance.close()
     return Promise.resolve(response)
   },
   function (error) {
